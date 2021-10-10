ca300423bf2d3bffe205bf3cc0d0d2375b1e91d4
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-10 13:55:41 +0300

    Create add row btn & styles

--- dist/script.js
+++ dist/script.js
@@ -8268,11 +8268,17 @@ const contentTable = document.querySelector(".note-table");
 const contentTableArchive = document.querySelector(".note-table-main");
 const contentTableCurent = document.querySelector(".note-table-archive");
 const switchArchive = document.querySelector(".toArchive");
+const addRowBtn = document.querySelector(".add-btn");
+const addRowRow = document.querySelector(".table-add-row");
 switchArchive.addEventListener("click", e => {
   contentTableArchive.classList.toggle('tbody-hidden');
   contentTableCurent.classList.toggle('tbody-hidden');
   switchArchive.classList.toggle('active');
   console.log("### tbales");
+});
+addRowBtn.addEventListener("click", () => {
+  addRowRow.classList.toggle("active");
+  addRowBtn.innerText = addRowRow.classList.contains("active") ? "Cancel" : "Add a row";
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ const contentTable = document.querySelector(".note-table");
 const contentTableArchive = document.querySelector(".note-table-main");
 const contentTableCurent = document.querySelector(".note-table-archive");
 const switchArchive = document.querySelector(".toArchive");
+const addRowBtn = document.querySelector(".add-btn");
+const addRowRow = document.querySelector(".table-add-row")
+
 
 switchArchive.addEventListener("click", (e) => {
     contentTableArchive.classList.toggle('tbody-hidden');
@@ -12,3 +15,8 @@ switchArchive.addEventListener("click", (e) => {
     switchArchive.classList.toggle('active');
     console.log("### tbales")
 });
+
+addRowBtn.addEventListener("click", () => {
+    addRowRow.classList.toggle("active");
+    addRowBtn.innerText = addRowRow.classList.contains("active") ? "Cancel" : "Add a row";
+})
