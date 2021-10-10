f8b634a6ff4ed89d215b44393d4c79b631843372
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-10 15:42:26 +0300

    Starter task external & genereate function

---
+++ src/js/modules/starterDate.js
@@ -0,0 +1,54 @@
+"use strict"
+
+const tasksList = [
+    {
+        name: "Pet the cat",
+        date: "10/01/2021",
+        category: "Random Thought",
+        content: "Find a cat, catch it, pet it",
+        dates: "",
+        status: 1,
+    },
+    {
+        name: "Feed ducks",
+        date: "10/05/2021",
+        category: "Random Thought",
+        content: "Take some breadcrumbs, find ducks, feed them",
+        dates: "",
+        status: 1,
+    },
+    {
+        name: "Pick raspberry",
+        date: "10/08/2021",
+        category: "Task",
+        content: "Go to the garden, and pick it",
+        dates: "",
+        status: 1,
+    },
+    {
+        name: "Build the house",
+        date: "10/09/2021",
+        category: "Task",
+        content: "Call to the builder teem in 10/10/2021",
+        dates: "10/10/2021",
+        status: 1,
+    },
+    {
+        name: "Visit a doctor",
+        date: "06/08/2021",
+        category: "Task",
+        content: "Visit a doctor. The dog was powerfull.",
+        dates: "",
+        status: 0,
+    },
+    {
+        name: "Kick naughty dog",
+        date: "05/08/2021",
+        category: "Random Thought",
+        content: "Find and kick it",
+        dates: "",
+        status: 0,
+    },
+];
+
+export default tasksList;
