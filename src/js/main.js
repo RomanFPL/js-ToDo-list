baf7ae866d2655ffe686ffcf2f551890d18fbb5a
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-10 14:29:57 +0300

    Add task data var

--- src/js/main.js
+++ src/js/main.js
@@ -21,3 +21,34 @@ addRowBtn.addEventListener("click", () => {
     addRowRow.classList.toggle("active");
     addRowBtn.innerText = addRowRow.classList.contains("active") ? "Cancel" : "Add a row";
 })
+
+const cuerentTask = [
+    {
+        name: "Pet the cat",
+        date: "10/01/2021",
+        category: "Random Thought",
+        content: "Find a cat, catch it, pet it",
+        dates: null,
+    },
+    {
+        name: "Feed ducks",
+        date: "10/05/2021",
+        category: "Random Thought",
+        content: "Take some breadcrumbs, find ducks, feed them",
+        dates: null,
+    },
+    {
+        name: "Pick raspberry",
+        date: "10/08/2021",
+        category: "Task",
+        content: "Go to the garden, and pick it",
+        dates: null,
+    },
+    {
+        name: "Bild the house",
+        date: "10/09/2021",
+        category: "Task",
+        content: "Call to the builder teem in 10/10/2021",
+        dates: "10/10/2021",
+    },
+];
