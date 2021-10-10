20051fd5e3421722ee2656e5162e2ce319925a3e
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-10 13:35:58 +0300

    Create Toggle To archive

--- dist/script.js
+++ dist/script.js
@@ -8264,6 +8264,16 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+const contentTable = document.querySelector(".note-table");
+const contentTableArchive = document.querySelector(".note-table-main");
+const contentTableCurent = document.querySelector(".note-table-archive");
+const switchArchive = document.querySelector(".toArchive");
+switchArchive.addEventListener("click", e => {
+  contentTableArchive.classList.toggle('tbody-hidden');
+  contentTableCurent.classList.toggle('tbody-hidden');
+  switchArchive.classList.toggle('active');
+  console.log("### tbales");
+});
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 "use strict";
 import 'bootstrap';
+
+const contentTable = document.querySelector(".note-table");
+const contentTableArchive = document.querySelector(".note-table-main");
+const contentTableCurent = document.querySelector(".note-table-archive");
+const switchArchive = document.querySelector(".toArchive");
+
+switchArchive.addEventListener("click", (e) => {
+    contentTableArchive.classList.toggle('tbody-hidden');
+    contentTableCurent.classList.toggle('tbody-hidden');
+    switchArchive.classList.toggle('active');
+    console.log("### tbales")
+});
